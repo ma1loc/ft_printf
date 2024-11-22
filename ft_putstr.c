20a50d2d7cc5d9e86a0fa6e7d6c006e409ec620a
ma1loc <anayouness51@gmail.com>
2024-11-22 21:30:53 +0100

    ft_putstr funtion

---
+++ ft_putstr.c
@@ -0,0 +1,15 @@
+#include "ft_printf.h"
+
+void    ft_putstr(char *s)
+{
+    size_t i;
+
+    i = 0;
+    // if (!s)
+    //     return ;
+    while(s[i])
+    {
+        ft_putchar(s[i]);
+        i++;
+    }
+}
