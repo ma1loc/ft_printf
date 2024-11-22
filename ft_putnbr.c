46714fd5d0e54d6ad2bca5edb118941657b692fc
ma1loc <anayouness51@gmail.com>
2024-11-22 21:30:02 +0100

    ft_putnbr function

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -3,30 +3,24 @@
 void    ft_putnbr(int nbr)
 {
     char c;
-    // use putstr
     if (nbr == -2147483648)
     {
-        write(1, "-2147483648", 11);
+        ft_putstr("-2147483648");
         return ;
     }
     if (nbr < 0)
     {
-        putchar('-');
+        ft_putchar('-');
         nbr = -nbr;
     }
     if (nbr <= 9)
     {
-        c = nbr + 0;
-        putchar(c);
+        c = (nbr + '0');
+        ft_putchar(c);
     }
     else
     {
         ft_putnbr(nbr / 10);
         ft_putnbr(nbr % 10);
     }
-}
-
-int main()
-{
-    ft_putnbr(10);
 }
