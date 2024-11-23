370c91dbdc80d4997b3b194f9eab3781e6d0bb32
ma1loc <anayouness51@gmail.com>
2024-11-23 13:27:20 +0100

    hex function

---
+++ ft_puthex.c
@@ -0,0 +1,22 @@
+#include "ft_printf.h"
+
+// for the (%x) not the (%X).
+// ======> test this <======
+
+void ft_put_hex(int n, char c)
+{    
+    char *hex_based;
+
+    // printf("the hex -> %d\n", n);
+    if (c == 'x')
+        hex_based = "0123456789abcdef";
+    if (c == 'X')
+        hex_based = "0123456789ABCDEF";
+    if (n >= 0 && n <= 15)
+        ft_putchar(hex_based[n]);
+    else
+    {
+        ft_hexadecimal(n / 16);
+        ft_hexadecimal(n % 16);
+    }
+}
