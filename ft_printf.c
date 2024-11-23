5e28c347ce38443a41be62257c6e09600f54124c
ma1loc <anayouness51@gmail.com>
2024-11-23 13:26:50 +0100

    ft_printf function

--- ft_printf.c
+++ ft_printf.c
@@ -15,16 +15,16 @@
 // va_arg   ->  Retrieves the next argument of the specified type.
 // va_end	->  Cleans up the va_list.
 
-int    ft_printf(char *s,int h, char ss, ...)
+int    ft_printf(char *s, ...)
 {
     va_list arg;
-    va_start(arg, ss);
-    printf("gp offset: %u\n", arg->gp_offset);
-    printf("fp offset: %u\n", arg->fp_offset);
-    printf("reg: %p\n", arg->reg_save_area);
-    printf("stack: %p\n", arg->overflow_arg_area);
+    va_start(arg, s);
+    // printf("gp offset: %u\n", arg->gp_offset);
+    // printf("fp offset: %u\n", arg->fp_offset);
+    // printf("reg: %p\n", arg->reg_save_area);
+    // printf("stack: %p\n", arg->overflow_arg_area);
     int idx = 0;
-    int count = 0;
+    // int count = 0;
     while (s[idx])
     {
         if (s[idx] == '%')
@@ -35,19 +35,19 @@ int    ft_printf(char *s,int h, char ss, ...)
                 ft_putchar(va_arg(arg, int));
             if (s[idx] == 's')                    // if it string -> %s
                 ft_putstr(va_arg(arg, char *));
-            // if (s[idx] == 'p')                    // if the string -> %p
+            // if (s[idx] == 'p')                 // if the string -> %p
             // noting yet   
             if (s[idx] == 'd' || s[idx] == 'i')   // if it digit  -> %d or %i
                 ft_putnbr(va_arg(arg, int));
-            // if (s[idx] == 'u')                    // if it udigit -> %u
-            // noting yet
-            
             // the hexadecimal (base 16) -> {0-9, A-F}
+            if (s[idx] == 'x' || s[idx] == 'X')                    // if it hexadecimal lower -> %x
+                ft_put_hex(va_arg(arg, int), s[idx]);
+            // if (s[idx] == 'X')                 // if it hexadecimal lower -> %x
+            // noting yet
+            if (s[idx] == 'u')                    // if it udigit -> %u
+                ft_put_unint(va_arg(arg, int));
             
-            // if (s[idx] == 'x')                    // if it hexadecimal lower -> %x
-            // noting yet
-            // if (s[idx] == 'X')                    // if it hexadecimal lower -> %x
-            // noting yet
+            
             // the (%%) ?
         }
         else
@@ -55,7 +55,7 @@ int    ft_printf(char *s,int h, char ss, ...)
         idx++;
         // count++;
     }
-    return (count);
+    // return (count);
 }
 
 int main()
