70f3a81f7c7701615402a8fa7c8011644e8dfe07
ma1loc <anayouness51@gmail.com>
2024-11-23 13:28:15 +0100

    un_int function

---
+++ ft_putunint.c
@@ -0,0 +1,17 @@
+#include "ft_printf.h"
+
+void    ft_put_unint(unsigned int n)
+{
+    char c;
+
+    if (n <= 9)
+    {
+        c = (n + '0');
+        ft_putchar(c);
+    }
+    else
+    {
+        ft_putnbr(n / 10);
+        ft_putnbr(n % 10);
+    }
+}
