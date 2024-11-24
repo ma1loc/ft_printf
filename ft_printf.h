622f0adf38d76155db63fcd02dc598f34785826b
ma1loc <anayouness51@gmail.com>
2024-11-24 11:23:27 +0100

    Header file

--- ft_printf.h
+++ ft_printf.h
@@ -4,13 +4,16 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-// test
+
 #include <stdio.h>
 
-// prototypes
+
+int     ft_printf(char *s, ...);
 void    ft_putchar(char c);
 void    ft_putstr(char *s);
+void    ft_putadd(unsigned long add);
 void    ft_putnbr(int n);
-void    ft_puthex(int n);
+void    ft_putunbr(unsigned int n);
+void    ft_puthex(int n, char c);
 
 #endif
