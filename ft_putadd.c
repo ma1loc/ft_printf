1023a2292d696da5fd73e679824297caba49392b
ma1loc <anayouness51@gmail.com>
2024-11-24 12:28:31 +0100

    putadd fun.

--- ft_putadd.c
+++ ft_putadd.c
@@ -1,34 +1,34 @@
 #include "ft_printf.h"
 
-void    ft_putadd(unsigned long add)
+void    ft_putadd(void *ptr)
 {
     //    46666664664666
     //    0x545af5454dd
     //  { 0x-----------}
-
-    printf("%lu", add);
+    unsigned long add;
     char *hex_based;
 
+    add = (unsigned long)ptr;
     hex_based = "0123456789abcdef";
 
     // put the 0x........
-    ft_putstr("0x");
+    if (add == (unsigned long)ptr) // check the tha address is the same?
+        ft_putstr("0x");
     
-    if (add >= 0 && add <= 15)
-        ft_putchar(hex_based[add]);
-    else
-    {
-        ft_putadd(add / 16);
-        // use putchar
-        ft_putadd(add % 16);
-    }
+    // if (add >= 0 && add <= 15)
+    //     ft_putchar(hex_based[add]);
+    // else
+    // {
+    //     putadd(add / 16);
+    //     ft_putadd(add % 16);
+    // }
+    printf("%lu", add);
 }
-
 
 int main()
 {
-    int a = 1;
+    char a = 'a';
 
-    printf("%p\n", &a);
+    // printf("printf->%p\n", &a);
     ft_putadd(&a);
 }
