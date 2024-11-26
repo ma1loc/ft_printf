8cd2c33930b040396632485d5c4bb200b2273e52
ma1loc <anayouness51@gmail.com>
2024-11-26 10:31:15 +0100

    printf fun.

--- ft_printf.c
+++ ft_printf.c
@@ -12,6 +12,28 @@
 
 #include "ft_printf.h"
 
+static int	ft_fscheck(va_list arg, char c)
+{
+	int	count;
+
+	count = 0;
+	if (c == 'c')
+		count += ft_putchar(va_arg(arg, int));
+	else if (c == 's')
+		count += ft_putstr(va_arg(arg, char *));
+	else if (c == 'd' || c == 'i')
+		count += ft_putnbr(va_arg(arg, int));
+	else if (c == 'x' || c == 'X')
+		count += ft_puthex(va_arg(arg, unsigned int), c);
+	else if (c == 'u')
+		count += ft_putuint(va_arg(arg, unsigned int));
+	else if (c == 'p')
+		count += ft_putadd(va_arg(arg, void *));
+	else
+		count += ft_putchar(c);
+	return (count);
+}
+
 int	ft_printf(const char *s, ...)
 {
 	va_list	arg;
