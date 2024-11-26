8d7fb96802f7e4afea841d98d8783cc645e06158
ma1loc <anayouness51@gmail.com>
2024-11-26 09:51:14 +0100

    printf fun.

--- ft_printf.c
+++ ft_printf.c
@@ -1,28 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanflous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 09:11:52 by yanflous          #+#    #+#             */
+/*   Updated: 2024/11/26 09:14:19 by yanflous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int    ft_printf(const char *s, ...)
+int	ft_printf(const char *s, ...)
 {
-    va_list arg;
-    int count;
-    int i;
+	va_list	arg;
+	int		count;
+	int		i;
 
-    va_start(arg, s);
-    i = 0;
-    count = 0;
-    
-    while (s[i])
-    {
-        if (s[i] == '%')
-        {
-            i++;
-            if (s[i] == '\0')
-                return (count);
-            count += ft_fscheck(arg, s[i]);
-        }
-        else
-            count += ft_putchar(s[i]);
-        i++;
-    }
-    va_end(arg);
-    return (count);
+	va_start(arg, s);
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == '%')
+		{
+			i++;
+			if (s[i] == '\0')
+				return (count);
+			count += ft_fscheck(arg, s[i]);
+		}
+		else
+			count += ft_putchar(s[i]);
+		i++;
+	}
+	va_end(arg);
+	return (count);
 }
